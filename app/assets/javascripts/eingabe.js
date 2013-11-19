f6cf9e6c86aec0a6e0a0e4ac9167f15f7061be50
Eckhard Hub <eckih@gmx.de>
2013-11-19 10:32:35 +0100

    eingabe.js: $.datepicker.setDefaults auskommentiert

--- app/assets/javascripts/eingabe.js
+++ app/assets/javascripts/eingabe.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	
 // Format für alle Datumsfelder
-$.datepicker.setDefaults({
-	dateFormat: "yy-mm-dd",
-	altFormat: "yy-mm-dd",
-	showOn: "both",
-	buttonImage: "/assets/calendar.gif",
-	buttonImageOnly: true});
+// $.datepicker.setDefaults({
+// 	dateFormat: "yy-mm-dd",
+// 	altFormat: "yy-mm-dd",
+// 	showOn: "both",
+// 	buttonImage: "/assets/calendar.gif",
+// 	buttonImageOnly: true});
 
 // Wenn sich das Datumsfeld ändert, sollen alle Datumsfelder voreingestellt werden
 $( "#tagderAblesung" ).datepicker({
