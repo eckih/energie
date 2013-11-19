d053faab59b4572d5d5c2fa051bee81d0e9c02b6
Eckhard Hub <eckih@gmx.de>
2013-11-19 09:37:55 +0100

    application.rb config.assets.initialize_on_precompile=false

--- config/initializers/better_errors.rb
+++ config/initializers/better_errors.rb
@@ -1,4 +1,4 @@
-# http://cristiandonosoc.wordpress.com/2013/06/20/better-errors-ubuntu-with-sublime-text-2-or-any-editor-for-that-matter/
+Add# http://cristiandonosoc.wordpress.com/2013/06/20/better-errors-ubuntu-with-sublime-text-2-or-any-editor-for-that-matter/
 BetterErrors.editor = :sublime if defined? BetterErrors
 
 
