a5a4799c512b3baf0d19440d2fa56c001ff52882
Eckhard Hub <eckih@gmx.de>
2013-11-19 10:36:18 +0100

    eingabe.js: alles auskommentiert

--- app/assets/javascripts/eingabe.js
+++ app/assets/javascripts/eingabe.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
-	
+/*	
 // Format für alle Datumsfelder
-// $.datepicker.setDefaults({
-// 	dateFormat: "yy-mm-dd",
-// 	altFormat: "yy-mm-dd",
-// 	showOn: "both",
-// 	buttonImage: "/assets/calendar.gif",
-// 	buttonImageOnly: true});
+$.datepicker.setDefaults({
+ 	dateFormat: "yy-mm-dd",
+ 	altFormat: "yy-mm-dd",
+ 	showOn: "both",
+ 	buttonImage: "/assets/calendar.gif",
+ 	buttonImageOnly: true});
 
 // Wenn sich das Datumsfeld ändert, sollen alle Datumsfelder voreingestellt werden
-$( "#tagderAblesung" ).datepicker({
-	dateFormat: "yy-mm-dd",
-	altFormat: "yy-mm-dd",
+
+ 	$( "#tagderAblesung" ).datepicker({
+ 		dateFormat: "yy-mm-dd",
+ 		altFormat: "yy-mm-dd",
 	altField: '.wdatum',
 	onSelect: function(){
 		$( ".wdatum" ).html($("#tagderAblesung").val());
@@ -28,7 +29,7 @@ $( "#tagderAblesung" ).datepicker("setDate", new Date);
 
 // Alle Datumsfelder voreinstellen
 $( ".wdatum" ).val($("#tagderAblesung").val());
-
+*/
 
 
 /*
