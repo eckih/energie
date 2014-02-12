ca5480c47c21111194a7151e1d820ff9fc4ce201
Eckhard Hub <eckih@gmx.de>
2014-02-12 16:02:46 +0100

    v0.8.4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Energie::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile += ['zaehlers.js.erb']
 
   # Generate digests for assets URLs
   config.assets.digest = true
