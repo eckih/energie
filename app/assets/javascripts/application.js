d68f786945a8c0b7ad8dd8c7bc9a52167cc464e1
Eckhard Hub <eckih@gmx.de>
2014-02-12 15:12:38 +0100

    v0.8.3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,5 @@
 //= require jquery_ujs
 //= jquery.purr
 //= require best_in_place
-//= require highcharts
-//= require_tree .
   
 // nach http://www.blogrammierer.de/jquery-ui-datepicker-in-deutscher-sprache/
