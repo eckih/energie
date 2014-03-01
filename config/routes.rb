c226ad0ac35ada0948daad9aef93eb3d440e9b82
Eckhard Hub <eckih@gmx.de>
2014-03-01 10:25:25 +0100

    neuer View grafik

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Energie::Application.routes.draw do
   resources :zaehlers do
     collection do
       get 'normiertewerte'
+      get 'grafik'
     end
   end
 
@@ -72,7 +73,7 @@ Energie::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'zaehlers#index'
+  root :to => 'zaehlers#grafik'
 
   # See how all your routes lay out with "rake routes"
 
